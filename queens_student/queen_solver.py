0dacbf3d6a032bb6234bdf70488b3925822f5b40
Robin <robin-be@hotmail.com>
2022-05-10 19:18:28 +0200

    add row and col clauses (to verify)

--- queens_student/queen_solver.py
+++ queens_student/queen_solver.py
@@ -1,3 +1,4 @@
+#import numpy as np
 from clause import *
 
 """
@@ -14,15 +15,118 @@ You should build clauses using the Clause class defined in clause.py
 
 Read the comment on top of clause.py to see how this works.
 """
-
+def print_board(matrix,queens):
+    for i,(x,y) in enumerate(queens):
+        for a, mat in enumerate(matrix):
+            for b, matt in enumerate(matrix):
+                if(x == b and y == a):
+                    matrix[a][b] = "1"
+    print(matrix)  
 
 def get_expression(size, queens=None):
     expression = []
-    for position in queens:
+    #matrix = np.array(np.zeros((size,size)))
+    #print("Matrix preview")
+    #print_board(matrix,queens)
+    cols = []
+    rows = []
+    for q in queens:
+        cols.append(q[0])
+        rows.append(q[1])
+
+    #check cols
+    for x in range(size):
         clause = Clause(size)
-        clause.add_positive(position[0],position[1])
-        print("clause",clause,": minisat",clause.minisat_str())
+        #Dans le cas ou aucune reine ne se situe sur cette col
+        if x not in cols:
+            # Clause avec seulement des positifs
+            for y in range(size):
+                clause.add_positive(x,y)
+            # print("\nclause",clause)
+
+            # Sous clause avec la negation de chacun des termes
+            for y in range(size-1):
+                for z in range(size-1):
+                    # Ajouter les clauses sans doublons
+                    if((x,y) != (x,z+1)):
+                        clause2 = Clause(size)
+                        clause2.add_negative(x,y)
+                        clause2.add_negative(x,z+1)
+                        expression.append(clause2)
+                        # print("sous clause",clause2)
+
+        # Dans le cas ou une reine se situe deja sur cette col
+        else:
+            for y in range(size):
+                if (x,y) in queens:
+                    clause.add_positive(x,y)
+                else:
+                    clause.add_negative(x,y)
+            # print("\nclause",clause)
+                
         expression.append(clause)
+    print()
+
+    print("#####\t Clauses by rows\t#####")
+    #check rows
+    for y in range(size):
+        clause = Clause(size)
+        #Dans le cas ou aucune reine ne se situe sur cette row
+        if y not in rows:
+            # Clause avec seulement des positifs
+            for x in range(size):
+                clause.add_positive(x,y)
+            print("\nclause",clause)
+
+            # Sous clause avec la negation de chacun des termes
+            for x in range(size-1):
+                for z in range(size-1):
+                    # Ajouter les clauses sans doublons
+                    if((x,y) != (z+1,y)):
+                        clause2 = Clause(size)
+                        clause2.add_negative(x,y)
+                        clause2.add_negative(z+1,y)
+                        expression.append(clause2)
+                        print("sous clause",clause2)
+
+        # Dans le cas ou une reine se situe deja sur cette row
+        else:
+            for x in range(size):
+                if (x,y) in queens:
+                    clause.add_positive(x,y)
+                else:
+                    clause.add_negative(x,y)
+            print("\nclause",clause)
+                
+        expression.append(clause)
+    print()
+    
+ 
+    # Check upper diagonal on left side
+    # for y in range(size):
+    #     for x in range(size):
+    #         clause = Clause(size)
+    #         for i, j in zip(range(x, -1, -1),
+    #                         range(y, -1, -1)):
+    #             if (i,j) in queens:
+    #                 clause.add_positive(x,y)
+    #             else:
+    #                 clause.add_negative(x,y)
+    #         # print("clause",clause)
+    #         expression.append(clause)
+
+    # Check lower diagonal on left side
+    # for y in range(size):
+    #     for x in range(size):
+    #         clause = Clause(size)
+    #         for i, j in zip(range(x, size, -1),
+    #                         range(y, -1, -1)):
+    #             if (i,j) in queens:
+    #                 clause.add_positive(x,y)
+    #             else:
+    #                 clause.add_negative(x,y)
+    #         # print("clause",clause)
+    #         expression.append(clause)
     return expression
 
 
